5b7570fda62f87c349752e74fee700776a74f5f7
HiroIshida <spitfire.docomo@gmail.com>
2019-10-21 03:29:33 +0900

    init

---
+++ gp.py
@@ -0,0 +1,135 @@
+import numpy as np
+import warnings
+from numpy.linalg import inv, det
+from numpy import trace
+from math import *
+from time import time
+from scipy.optimize import fmin_l_bfgs_b
+from copy import deepcopy
+
+class GaussianProcess:
+    def __init__(self, X, Y, kernel):
+        print "hoge"
+
+        def dimentions():
+            x0 = X[0]
+            if type(x0) == tuple: # input with covariance info
+                return x0[0].size
+            return x0.size
+
+        self.n_dim = dimentions()
+        self.kernel = kernel
+        self.X = X
+        self.Y = Y
+        self.n_train = len(X)
+        self.construct_matrixes()
+
+    def predict(self, x):
+
+        withoutInpUnc = (type(x) is not tuple)
+        if withoutInpUnc:
+            x = (x, np.zeros((self.n_dim, self.n_dim)))
+
+        K_s = self._construct_K_s(x)
+        K_s_tr = K_s.transpose()
+        K_ss = np.matrix(self.kernel.k(x, x))
+
+        Y_vec = np.matrix(self.Y).transpose()
+
+        gp_mean = (np.dot(np.dot(K_s_tr, self.K_inv), Y_vec)).item()
+        gp_var = (K_ss - np.dot(np.dot(K_s_tr, self.K_inv), K_s)).item()
+        if gp_var < 0:
+            gp_var = 0
+        return gp_mean, gp_var
+
+    def _construct_K_s(self, x):
+        K_s = np.zeros((self.n_train, 1))
+        for i in range(self.n_train):
+            x_i = self.X[i]
+            K_s[i] = self.kernel.k(x_i, x)
+        return K_s
+
+    def optimize(self, params_min = None, params_max = None, dump_log = False):
+        fun = lambda x: - self.compute_likelihood(x, params_min, params_max) # to maximize it via minimize function
+        grad = lambda x: - self.compute_likelihood_grad(x) # to maximize it via minimize function
+        tes = fmin_l_bfgs_b(fun, self.kernel.params, fprime=grad)
+
+    def compute_likelihood_grad(self, params_ = None, params_min = None, params_max = None):
+        K_inv_Y = self.K_inv.dot(np.matrix(self.Y).transpose())
+        n_params = self.kernel.n_params
+
+        grad = np.zeros(n_params)
+        for n in range(n_params):
+            K_grad = self.K_grad_lst[n]
+            grad[n] = - trace(self.K_inv.dot(K_grad)) + K_inv_Y.transpose().dot(K_grad).dot(K_inv_Y)
+        return grad
+
+    def compute_likelihood(self, params_ = None, params_min = None, params_max = None):
+
+        if params_ is not None:
+            # when this function is called in the optimizer (e.g. scipy.optimize.fmin_l_bfgs()) ,
+            # params_ is sets, which tells the kernel that parameters are changed.
+            # Then reconstruct the covariance matrix again.
+            #if GaussianProcess
+
+            params = list(params_) # in case given in numpy.array
+
+            def isValidParams():
+                for i in range(len(params)):
+                    if params_min is not None:
+                        if params[i] < log(params_min[i]):
+                            return False
+                    if params_max is not None:
+                        if params[i] > log(params_max[i]):
+                            return False
+                return True
+
+            if isValidParams():
+                self.kernel.set_param(params) 
+                self.construct_matrixes()
+            else:
+                print [exp(p) for p in params]
+                print "PARAMETERS HIT THE BOUND"
+
+
+        Y_vec = np.matrix(self.Y).transpose()
+        datafit = - Y_vec.transpose().dot(self.K_inv).dot(Y_vec).item()
+        simpleness = - log(det(self.K))
+        likelihood = datafit + simpleness
+        return likelihood
+
+    def construct_matrixes(self):
+        # K and K_grad (derivative of K w.r.t hyperparameters theta) will be constructed and cached
+        # the reason why constructed both K and K_grad in the same method is that 
+        # some terms appears in the computation of K will also appear in K_grad, so 
+        # if we devide this function into two, then we have to do the same computation for
+        # some terms twice
+
+        K = np.zeros((self.n_train, self.n_train))
+
+        # note that gradient matrix of K must be constructed for each parameter; that's why the following is a list
+        K_grad_lst = [np.zeros((self.n_train, self.n_train)) for i in range(self.kernel.n_params)]
+
+        for i in range(self.n_train):
+            for j in range(self.n_train):
+                k, k_grad = self.kernel.k(self.X[i], self.X[j], with_grad = True)
+
+                K[i][j] = k
+                for n in range(self.kernel.n_params):
+                    K_grad_lst[n][i][j] = k_grad[n]
+
+        K_inv = inv(K)
+
+        # Note that it is worth cacheng K_inv; computation of this takes time, and will be used entirely in this class
+        self.K_inv = K_inv 
+        self.K = K
+        self.K_grad_lst = K_grad_lst
+
+    def get_boundary(self, margin = 0.0):
+        x_lst_lst = [[self.X[i][0][j] for i in range(self.n_train)] for j in range(self.n_dim)]
+        bmin_ = np.array([min(x_lst) for x_lst in x_lst_lst])
+        bmax_ = np.array([max(x_lst) for x_lst in x_lst_lst])
+        dif = bmax_ - bmin_
+        bmin = bmin_ - margin * dif
+        bmax = bmax_ + margin * dif
+        return bmin, bmax
---
+++ kernel.py
@@ -0,0 +1,85 @@
+import numpy as np
+from numpy.random import rand
+import warnings
+import matplotlib.pyplot as plt
+from numpy.linalg import inv, det
+import numpy.linalg as la
+from numpy import trace
+from math import *
+from copy import deepcopy
+
+class matern23_uncertain:
+    def __init__(self, l = 1.0, noise = 0.01, dim = 2, N = 100, aniso = None):
+        # aniso (list) is anisotropic factor 
+        params = [log(l), log(noise)]
+        self.set_param(params)
+        self.n_params = len(params)
+        self.N = N # mc sample number
+        self.dim = dim
+
+        if aniso is None:
+            self.aniso = np.ones(dim)
+        else:
+            if dim > 1:
+                self.aniso = np.array(aniso)
+            else:
+                error(" ")
+
+        ## pre-generate random vector for a faster online computation
+        if dim == 1:
+            self.rns1 = np.random.randn(N)
+            self.rns2 = np.random.randn(N)
+        else:
+            self.rns1 = np.random.randn(dim, N)
+            self.rns2 = np.random.randn(dim, N)
+
+    def set_param(self, params):
+        self.params = params # is not used inside 
+        self.l = exp(params[0])
+        self.noise = exp(params[1])
+
+    def k(self, x1, x2, with_grad = False):
+        mu1, cov1 = x1
+        mu2, cov2 = x2
+
+        boolean = np.array_equal(mu1, mu2)
+
+
+        if self.dim == 1:
+            pts1 = sqrt(cov1)*self.rns1 + mu1
+            pts2 = sqrt(cov2)*self.rns2 + mu2 
+            dif = pts1 - pts2
+            r_vec = np.abs(dif)
+        else:
+            if np.sum(cov1) == 0.0:
+                pts1 = np.tile(mu1, [self.N, 1]).T
+            else:
+                L1 = np.linalg.cholesky(cov1)
+                pts1 = np.dot(L1, self.rns1) + np.tile(mu1, [self.N, 1]).T
+
+            if np.sum(cov2) == 0.0:
+                pts2 = np.tile(mu2, [self.N, 1]).T
+            else:
+                L2 = np.linalg.cholesky(cov2)
+                pts2 = np.dot(L2, self.rns2) + np.tile(mu2, [self.N, 1]).T
+
+            dif = pts1 - pts2
+            # add anisotopic factor
+            print self.aniso
+            for i in range(self.dim):
+                dif[i, :] /= self.aniso[i]
+            r_vec = np.sqrt(np.sum(dif**2, axis = 0))
+
+        # calc  
+        exps = np.exp(-sqrt(3)*r_vec/self.l)
+        kern_vector =  (1 + sqrt(3)*r_vec/self.l) * exps
+        kern = np.mean(kern_vector)
+
+        if not with_grad:
+            return kern
+
+        left = -sqrt(3)*r_vec/self.l**2 + (1 + sqrt(3)*r_vec/self.l) * (sqrt(3)*r_vec/self.l**2)
+        grad0 = np.mean(left * exps)
+        grad1 = self.noise * boolean
+        kern_grad = [grad0, grad1]
+        return kern, kern_grad
